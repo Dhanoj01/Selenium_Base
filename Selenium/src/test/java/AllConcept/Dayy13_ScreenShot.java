50c5a764dd78418d1a4dc09808dbb68ed47f6589
dhanojkumarsingh <dhanojkumar783@gmail.com>
2026-06-01 18:01:41 +0530

    Screen Shot
    1. take Screenshot
    2. save screenshot
    
    source
    destination
    copy

---
+++ Selenium/src/test/java/AllConcept/Dayy13_ScreenShot.java
@@ -0,0 +1,39 @@
+package AllConcept;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dayy13_ScreenShot {
+
+    public static void main(String[] args) throws Exception {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.example.com/");
+
+        // Take Screenshot
+        TakesScreenshot ts = (TakesScreenshot) driver;
+
+        File source = ts.getScreenshotAs(OutputType.FILE);
+
+        // Save Screenshot
+        File destination = new File("screenshot.png");
+
+        Files.copy(
+                source.toPath(),
+                destination.toPath(),
+                StandardCopyOption.REPLACE_EXISTING);
+
+        System.out.println("Screenshot Taken Successfully");
+
+        driver.quit();
+    }
+}
